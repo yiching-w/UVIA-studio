8d250137a5671e399ea3c099bb23fba658ff8b0a
Yi Ching Wang <doo8409255@gmail.com>
2022-07-02 01:00:11 +0800

    gallery bug

--- assets/script/main.js
+++ assets/script/main.js
@@ -188,12 +188,12 @@
         buttons: ["close"]
     });
 
-    [].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
-        img.setAttribute('src', img.getAttribute('data-src'));
-        img.onload = function () {
-            img.removeAttribute('data-src');
-        };
-    });
+    // [].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
+    //     img.setAttribute('src', img.getAttribute('data-src'));
+    //     img.onload = function () {
+    //         img.removeAttribute('data-src');
+    //     };
+    // });
 
     $('.floatingButton').on('click',
     function(e) {
